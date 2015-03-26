dbd2bbb9a1e262acc6a0a62359b32ad1bd25c24c
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-03-26 03:20:31 -0600

    UI tweaks to video player.

--- practica3/www/js/controllers.js
+++ practica3/www/js/controllers.js
@@ -65,13 +65,9 @@ angular.module('starter.controllers', [])
 
     // Buttons
     var playButton = document.getElementById("play-pause");
-    var muteButton = document.getElementById("mute");
-    var fullScreenButton = document.getElementById("full-screen");
 
     // Sliders
     var seekBar = document.getElementById("seek-bar");
-    var volumeBar = document.getElementById("volume-bar");
-
     var container = document.getElementById("video-container")
 
     container.addEventListener("click", function() {
@@ -95,37 +91,6 @@ angular.module('starter.controllers', [])
         playButton.innerHTML = "Play";
       }
     });
-
-
-    // Event listener for the mute button
-    muteButton.addEventListener("click", function() {
-      if (video.muted == false) {
-        // Mute the video
-        video.muted = true;
-
-        // Update the button text
-        muteButton.innerHTML = "Unmute";
-      } else {
-        // Unmute the video
-        video.muted = false;
-
-        // Update the button text
-        muteButton.innerHTML = "Mute";
-      }
-    });
-
-
-    // Event listener for the full-screen button
-    fullScreenButton.addEventListener("click", function() {
-      if (video.requestFullscreen) {
-        video.requestFullscreen();
-      } else if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen(); // Firefox
-      } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen(); // Chrome and Safari
-      }
-    });
-
 
     // Event listener for the seek bar
     seekBar.addEventListener("change", function() {
@@ -154,12 +119,6 @@ angular.module('starter.controllers', [])
     // Play the video when the seek handle is dropped
     seekBar.addEventListener("mouseup", function() {
       video.play();
-    });
-
-    // Event listener for the volume bar
-    volumeBar.addEventListener("change", function() {
-      // Update the video volume
-      video.volume = volumeBar.value;
     });
   })
 
